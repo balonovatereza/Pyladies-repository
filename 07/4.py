8e86c9d480ac76e4eae4fe0f853bf497b3305ef7
Tereza Balonová <balonova.tereza@seznam.cz>
2019-03-24 19:32:16 +0100

    Presun do slozky

--- 07/4.py
+++ 07/4.py
@@ -9,7 +9,10 @@ def vrat_seznamy(seznam1, seznam2):
     zvířata, která jsou jen v prvním seznamu (rozdíl množin: první - druhá),
     zvířata, která jsou jen ve druhém seznamu (rozdíl množin: druhá - první).
     '''
-    sjednocene = seznam1 + seznam2
+    sjednocene = []
+    for zvire in seznam2 and seznam1:
+        if zvire not in sjednocene:
+            sjednocene.append(zvire)
     rozdil1 = list(set(seznam1) - set(seznam2))
     rozdil2 = list(set(seznam2) - set(seznam1))
     return sjednocene, rozdil1, rozdil2
